6122c56018be73d7b7452d97669092ad9f288a7d
suptoasty <jason.lonsinger@gmail.com>
2021-06-02 15:35:57 -0500

    setup fauna vercel integration

---
+++ api/collections.js
@@ -0,0 +1,28 @@
+import faunadb, { query } from "faunadb"
+
+const { FAUNADB_SECRET: secret } = process.env
+
+let client
+
+if (secret) {
+	client = new faunadb.Client({ secret })
+}
+
+export default async (req, res) => {
+	try {
+		let collections = []
+
+		if (!client) {
+			return res
+				.status(500)
+				.json({ error: new Error("Missing secret to connect to FaunaDB") })
+		}
+
+		await client
+			.paginate(query.Collections())
+			.map((ref) => query.Get(ref))
+			.each((page) => (collections = collections.concat(page)))
+	} catch (error) {
+		res.status(500).json({ error })
+	}
+}

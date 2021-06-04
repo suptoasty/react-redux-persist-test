233a444400ace3036ba8c1d9c60d00301351fee3
suptoasty <jason.lonsinger@gmail.com>
2021-06-04 16:13:32 -0500

    Added Testing. Moved Deps to Dev.

---
+++ test/counter.test.tsx
@@ -0,0 +1,10 @@
+// import Counter from "@/features/counter/counter"
+// import { mount } from "enzyme"
+
+describe("Counter Component", () => {
+	it.todo("Counter Renders")
+	// it("Counter Renders", () => {
+	// 	const wrapper = mount(<Counter />)
+	// 	// expect(wrapper.count).toBe(0)
+	// })
+})
